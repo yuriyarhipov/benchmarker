15833dcd3a48bb603b74a7f57bb7883cbdc015a4
Yuriy Arhipov <yuriyarhipovua@yandex.ru>
2015-05-27 10:02:22 +0300

    [#18] support office xml

--- lib/excel.py
+++ lib/excel.py
@@ -1,4 +1,5 @@
 from openpyxl import load_workbook
+from lxml import etree
 import xlrd
 
 class Excel(object):
@@ -22,16 +23,31 @@ class Excel(object):
                         row.append('')
                 data.append(row)
         else:
-            workbook = xlrd.open_workbook(self.filename)
-            worksheet = workbook.sheet_by_index(0)
-            data = []
-            for curr_row in range(worksheet.nrows):
-                data_row = []
-                row = worksheet.row(curr_row)
-                for curr_cell in range(worksheet.ncols):
-                    data_row.append(str(worksheet.cell_value(curr_row, curr_cell)))
-                data.append(data_row)
-            print len(data)
+            try:
+                workbook = xlrd.open_workbook(self.filename)
+                worksheet = workbook.sheet_by_index(0)
+                data = []
+                for curr_row in range(worksheet.nrows):
+                    data_row = []
+                    row = worksheet.row(curr_row)
+                    for curr_cell in range(worksheet.ncols):
+                        data_row.append(str(worksheet.cell_value(curr_row, curr_cell)))
+                    data.append(data_row)
+            except:
+                tree = etree.parse(self.filename)
+                root = tree.getroot()
+                for worksheet in root:
+                    if 'Worksheet' in worksheet.tag:
+                        for table in worksheet:
+                            if 'Table' in table.tag:
+                                for row in table:
+                                    data_row = []
+                                    if 'Row' in row.tag:
+                                        for cell in row:
+                                            for d in cell:
+                                                data_row.append(d.text)
+                                    data.append(data_row)
+
         return data
 
     def get_competitors_template(self):
