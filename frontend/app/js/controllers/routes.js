98ada76c4fdec20a9218f44a52bcf452de21b781
Yuriy Arhipov <yuriyarhipovua@yandex.ru>
2015-12-03 11:59:53 +0300

    leaflet

--- frontend/app/js/controllers/routes.js
+++ frontend/app/js/controllers/routes.js
@@ -77,7 +77,11 @@ routeControllers.controller('routeCtrl', ['$scope', '$http', '$routeParams', 'ac
             };
             leafletData.getMap().then(function(map) {
                 map.setView([latitude, longitude], 13);
-                var markers = L.markerClusterGroup();
+                var markers = L.markerClusterGroup({
+                    spiderfyOnMaxZoom: false,
+                    showCoverageOnHover: false,
+                    disableClusteringAtZoom: 17
+                });
                 for (id in data.route){
                     var circle = L.circle([data.route[id].lat, data.route[id].lon], 1, {
                         color: data.route[id].color,
